586fa020477471c7b0b0448478c8761d8ca62500
Sergey Kononenko <kononencheg@gmail.com>
2012-04-16 18:12:20 +0400

    main controller

--- application/control/main-controller.js
+++ application/control/main-controller.js
@@ -1,4 +1,9 @@
+
+
+
 /**
+ * Класс основного контроллера логики приложения.
+ *
  * @constructor
  * @extends {tuna.control.ViewController}
  */
@@ -6,11 +11,21 @@ var MainController = function() {
     tuna.control.ViewController.call(this);
 
     /**
-     * @override
+     * Список модулей-компонентов отображения, которые необходимо
+     * проинициализировать в контейнере соответствующему данному контроллеру
+     * отображения.
+     *
+     * Основному контроллеру отображения соотвествует базовый узел страницы
+     * приложения.
+     *
+     * @type {!Array.<string>}
+     * @protected
      */
     this._modules = [ 'popup' ];
 };
 
 tuna.utils.extend(MainController, tuna.control.ViewController);
 
+// Установка экземпляра класса <code>MainController</code> как основного
+// контроллера отображения.
 tuna.control.setMainController(new MainController());
